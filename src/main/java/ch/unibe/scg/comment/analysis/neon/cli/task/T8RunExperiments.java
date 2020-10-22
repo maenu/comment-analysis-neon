5e1b6dc2e34d25362eef61daa72e5bdf8fa92ccc
poojaruhal <poojaruhal65@gmail.com>
2020-10-22 22:04:12 +0200

    Add inline comments

--- src/main/java/ch/unibe/scg/comment/analysis/neon/cli/task/T8RunExperiments.java
+++ src/main/java/ch/unibe/scg/comment/analysis/neon/cli/task/T8RunExperiments.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /** Load all training (0-0-categoryName_feature) arff files, and train the mentioned classifiers in experiment.xml.
+ * Configures/Runs the Experiment (experiment.xml) for each feature set and set input and outfile .xml file.
  * @data language under analysis
  * @directory directory of the dataset where arff files and other intermediate data is saved
  */
@@ -35,13 +36,14 @@ public class T8RunExperiments {
 	public void run() throws IOException, InterruptedException, SQLException {
 		String experimentConfigurationTemplate = Utility.resource("experiment.xml");
 		ExecutorService executor = Executors.newFixedThreadPool(this.threads);
+		// prefix: <training/testing-category-featureset> eg., 0-0-licensecopyright-tfidf
 		for (String prefix : Files.list(this.directory)
 				.filter(p -> p.getFileName().toString().endsWith(".arff") && p.getFileName()
 						.toString()
 						.startsWith("0-0-"))
 				.map(p -> p.getFileName().toString().split("\\.")[0])
 				.collect(Collectors.toList())) {
-			// save experimenter configuration
+			// save experimenter configuration for each category
 			Path configuration = this.directory.resolve(Paths.get(String.format("%s.xml", prefix)));
 			Files.writeString(configuration,
 					experimentConfigurationTemplate.replaceAll("\\{\\{prefix}}",
@@ -52,6 +54,7 @@ public class T8RunExperiments {
 			executor.submit(() -> {
 				LOGGER.info("{} training {}...", this.data, prefix);
 				try {
+					// Configures/Runs the Experiment from the command line.
 					Experiment.main(new String[]{"-l", configuration.toFile().getAbsolutePath(), "-r"});
 					LOGGER.info("{} training {} finished", this.data, prefix);
 				} catch (Throwable e) {
