95fbf34fdc896b78bb431dacd22a86c02e4a2e8c
poojaruhal <poojaruhal65@gmail.com>
2022-12-03 17:08:03 +0100

    fix sql error in T5

--- src/main/java/ch/unibe/scg/comment/analysis/neon/cli/task/T5StorePartitionSentences.java
+++ src/main/java/ch/unibe/scg/comment/analysis/neon/cli/task/T5StorePartitionSentences.java
@@ -38,7 +38,7 @@ public class T5StorePartitionSentences {
 						ResultSet result = statement.executeQuery("SELECT class, comment_sentence, partition FROM " + this.data
 								+ "_3_sentence_mapping_clean JOIN " + this.data + "_4_sentence_partition_workshop on ("
 								+ this.data + "_4_sentence_partition_workshop.comment_sentence_id = " + this.data
-								+ "_3_sentence_mapping_clean.comment_sentence_id) WHERE category = \"" + category
+								+ "_3_sentence_mapping_clean.comment_sentence_id) WHERE "+ this.data + "_4_sentence_partition_workshop.category = \"" + category
 								+ "\"")
 				) {
 					while (result.next()) {
